73922a21b3405ce8db9e5f3a18011588a39e04de
preshykene <preciousekene51@gmail.com>
2023-09-11 09:06:17 +0000

    Done with this

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,21 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue({name: 'push_notification_code'});
+
+const job = queue.create('push_notification_code', {
+  phoneNumber: '08094333676',
+  message: 'This is the code to verify your account',
+});
+
+job
+  .on('enqueue', () => {
+    console.log('Notification job created:', job.id);
+  })
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed attempt', () => {
+    console.log('Notification job failed');
+  });
+job.save();
+
